97cddb9ca503eccda6628f7a430a7cd042ecfbc0
kolron <74140353+kolron@users.noreply.github.com>
2021-03-09 06:18:33 -0500

    removed magic numbers

--- Snowman/snowman.cpp
+++ Snowman/snowman.cpp
@@ -90,24 +90,48 @@ string make_body(string x)
 {
      std::string u_body = " (   )  \n";
      std::string l_body = " (   )  \n";
+
+     /*
+    x[0] = Nose
+    x[1] = Left eye
+    x[2] = right eye
+    x[3] = Left arm
+    x[4] = Right arm
+    x[5] = Torso
+    */
+    const int nose_input = 0;
+    const int nose_pos = 3;
+    const int l_eye_input = 1;
+    const int r_eye_input = 2;
+    const int l_arm_input = 3 ;
+    const int l_arm_pos = 0 ;
+    const int r_arm_input = 4 ;
+    const int r_arm_pos = 6;
+    const int torso_input = 5;
+    const int torso_mid_pos = 3;
+    const int torso_l_pos = 2;
+    const int torso_r_pos = 4;
+
+
+
    
     ////////////////
     // NOSE ////////
     ////////////////
-    switch (x[0]) 
+    switch (x[nose_input]) 
     {
     case '1':
-        u_body[3] = ',';
+        u_body[nose_pos] = ',';
         break;
     case '2':
-        u_body[3] = '.';
+        u_body[nose_pos] = '.';
      
     case '3':
-        u_body[3] = '_' ;  
+        u_body[nose_pos] = '_' ;  
         break;
 
     case '4':
-        u_body[3] = ' ' ;  
+        u_body[nose_pos] = ' ' ;  
         break;
 
     default:
@@ -118,21 +142,21 @@ string make_body(string x)
     ////////////////
     // TORSO ///////
     ////////////////
-    switch(x[5]) 
+    switch(x[torso_input]) 
     {
         case '1':
-            l_body[3] = ':';
+            l_body[torso_mid_pos] = ':';
             break;
         case '2':
-            l_body[2] = ']';
-            l_body[4] = '[';
+            l_body[torso_l_pos] = ']';
+            l_body[torso_r_pos] = '[';
             break;
         case '3':
-            l_body[2] = '>';
-            l_body[4] = '<';
+            l_body[torso_l_pos] = '>';
+            l_body[torso_r_pos] = '<';
             break;
         case '4':
-            l_body[3] = ' ';
+            l_body[torso_mid_pos] = ' ';
             break;
 
         default:
@@ -140,38 +164,29 @@ string make_body(string x)
     }
 
 
-    /*
-    x[0] = Nose
-    x[1] = Left eye
-    x[2] = right eye
-    x[3] = Left arm
-    x[4] = Right arm
-    x[5] = Torso
-    */
-
     ////////////////
     // BOTH EYES ///
     ////////////////
-    u_body = make_eyes(u_body, x[1], x[2]);
+    u_body = make_eyes(u_body, x[l_eye_input], x[r_eye_input]);
     
 
     ////////////////
     // LEFT ARM ////
     ////////////////
-    switch(x[3])
+    switch(x[l_arm_input])
     {
     case '1':
-        l_body[0] = '<';
+        l_body[l_arm_pos] = '<';
         break;
     case '2':
-        u_body[0] = '\\';
+        u_body[l_arm_pos] = '\\';
         break;
     case '3':
-        l_body[0] = '/';
+        l_body[l_arm_pos] = '/';
         break;
     
     case '4':
-         l_body[0] = ' ';
+         l_body[l_arm_pos] = ' ';
          break;
     default:
         exit(1);
@@ -180,34 +195,37 @@ string make_body(string x)
     ////////////////
     // RIGHT ARM ///
     ////////////////
-    cout << "here" << endl;
-    switch(x[4])
+    
+    switch(x[r_arm_input])
     {
     case '1':
-        l_body[6] = '>';
+        l_body[r_arm_pos] = '>';
         break;
     case '2':
-        u_body[6] = '/';
+        u_body[r_arm_pos] = '/';
         break;
     case '3':
-        l_body[6] = '\\';
+        l_body[r_arm_pos] = '\\';
         break;
      case '4':
-        l_body[6] = ' ';
+        l_body[r_arm_pos] = ' ';
         break;
 
     default:
         exit(1);
     }  
-    cout << u_body << endl;
    string body = u_body + l_body;
   
    return body;
 }
 
-string  make_base(char x)
+string make_base(char x)
 {
      std::string b_base = " (   )\n";
+     const int base_mid_pos = 3;
+     const int base_l_pos = 2;
+     const int base_r_pos = 4;
+
 
     ////////////////
     // BASE  ///////
@@ -216,17 +234,17 @@ string  make_base(char x)
       switch (x)
     {
     case '1':
-        b_base[3] = ':';
+        b_base[base_mid_pos] = ':';
         
         break;
     case '2':
-        b_base[2] = '"';
-        b_base[4] = '"';
+        b_base[base_l_pos] = '"';
+        b_base[base_r_pos] = '"';
        break;
     case '3':
-        b_base[2] = '_';
-        b_base[3] = '_';
-        b_base[4] = '_';
+        b_base[base_l_pos] = '_';
+        b_base[base_mid_pos] = '_';
+        b_base[base_r_pos] = '_';
       break;
     case '4':
 
@@ -242,17 +260,17 @@ string  make_base(char x)
 
 string snowman(int x)
 {
-   
+   const int length = 8;
     string input = to_string(x);
-    if (input.length() != 8) //if string is too short
+    if (input.length() != length) //if string is too short or too long
     {
             string _err_msg = ("Invalid code '" + input + "'");
             throw std::invalid_argument(_err_msg);
     }
 
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < length; i++)
     {
-        if(!(input[i] >= 49 && input[i] <= 52)) //if string has numbers(or characters for that matter) that are not 1,2,3,4
+        if(!(input[i] >= '1' && input[i] <= '4')) //if string has numbers(or characters for that matter) that are not 1,2,3,4
         {
             
             string _err_msg = ("Invalid code '" + input + "'");
@@ -260,17 +278,21 @@ string snowman(int x)
         }
     }
     
+    const int start_body = 1;
+    const int end_body = 6;
+    const int hat_input = 0;
+    const int base_input = 7;
+    
+    string body_input = input.substr(start_body,end_body);//just for easier work in make_body
 
-    string body_input = input.substr(1,6);//just for easier work in make_body
-
-    string final_snowman = make_hat(input[0]) + make_body(body_input) + make_base(input[7]);
+    string final_snowman = make_hat(input[hat_input]) + make_body(body_input) + make_base(input[base_input]);
   
-     cout << final_snowman << endl; //TODO: REMOVE IN THE END
+    //cout << final_snowman << endl; //TODO: REMOVE IN THE END
     return final_snowman;
 }
  };
 // int main()
 // {
-//     ariel::snowman(41234123);
+//     ariel::snowman(34123412);
 //     return 0;
 // }
