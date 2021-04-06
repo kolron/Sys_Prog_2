50dd1241b34abea7178fb6dc2de0c15c73621a46
kolron <74140353+kolron@users.noreply.github.com>
2021-04-06 15:34:09 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -22,6 +22,7 @@ namespace ariel
 
         board=vector<vector<char>>(rowEdge, vector<char>(colEdge, '_'));
     }
+  
     void Board::post(unsigned int row, unsigned int col, Direction direction, string msg)
     {
       
@@ -33,9 +34,6 @@ namespace ariel
         //make sure board size fits
         fitSize(row, col, msglen, direction);
        
-       
-       
-
         for( size_t i = 0; i<msglen; i++)
         {
             //handle a board that is past max size()
@@ -73,10 +71,26 @@ namespace ariel
             }
            
         }
-       
-      
     }
 
+
+     unsigned int Board:: rec_row()
+    {
+        if (rowEdge < UINT_MAX)
+        {   
+          return rowEdge;
+        }
+         return 0; 
+        
+    }
+      unsigned int Board::rec_col()
+    {
+        if (colEdge < UINT_MAX)
+        {   
+          return colEdge;
+        }
+         return 0; 
+    }
 
     string Board::read(unsigned int row, unsigned int col, Direction direction, unsigned int len)
     {
@@ -112,25 +126,16 @@ namespace ariel
     {
         if(flag_written)
         {
-        unsigned int actualHeight = rowEdge - firstRow+1;
-      
-        unsigned int actualWidth = colEdge - firstCol+1; 
-        unsigned int rowNum = firstRow-1;
-        for (unsigned int i = 0; i < actualHeight; i++)
-        {
-            if(rowNum < required_2_spaces)
+            unsigned int actualHeight = rowEdge - firstRow+1;
+            unsigned int actualWidth = colEdge - firstCol+1; 
+            unsigned int rowNum = firstRow;
+            for (unsigned int i = 0; i < actualHeight; i++)
             {
-
-                cout<<rowNum<< ":" << setw(3) <<"|"<<read(firstRow-(unsigned int)1+i, firstCol, Direction::Horizontal, actualWidth) << "|" << endl;
+           
+                cout <<"|"<<read(firstRow-(unsigned int)1+i, firstCol, Direction::Horizontal, actualWidth) << "|" << ":" << rowNum <<  endl;
                 rowNum++;
-            }
-            else
-            {
-                cout<< rowNum<< ":" << setw(2) <<"|"<<read(firstRow-(unsigned int)1+i, firstCol, Direction::Horizontal, actualWidth) << "|" << endl;
-                rowNum++;
-            }
          
-        }
+            }
         }
         else
         {
@@ -155,7 +160,15 @@ namespace ariel
         {
             board[i].resize(colEdge, '_');
         }
-     
+    }
+
+     char Board::check_direction(char c)
+    {
+        if (c == 'v' || c == 'h' || c == 'V' || c == 'H' || c == 'e' || c == 'E')
+        {
+            return c;
+        }
+        return 0;
     }
 
 
